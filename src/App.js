4a86aec643c1915f7f5deb119572735278cc7fa1
Yousuf Qutubuddin <usuf52@gmail.com>
2021-02-12 20:48:46 +0500

    Setup React Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Provider } from "react-redux";
+import { ConfigureStore } from "./Redux/Store";
+import Routes from "./Routes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Talentadore-App">
+      <Provider store={ConfigureStore()}>
+        <Routes />
+      </Provider>
     </div>
   );
 }
