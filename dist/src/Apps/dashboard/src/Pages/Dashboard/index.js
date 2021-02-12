5d594ccee3c737fac45341afe419bbb96dd85a57
Yousuf Qutubuddin <usuf52@gmail.com>
2021-02-12 20:54:25 +0500

    Bit Build FIles

---
+++ dist/src/Apps/dashboard/src/Pages/Dashboard/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _jsxRuntime = require("react/jsx-runtime");
+
+var _default = function _default() {
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)("h1", {
+    children: "Dashboard"
+  });
+};
+
+exports.default = _default;
+
+//# sourceMappingURL=index.js.map
